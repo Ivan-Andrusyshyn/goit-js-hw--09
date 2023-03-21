6f1987bd6eb96949932fb193f6fe7bd4eba694ae
Ivan Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-03-21 22:13:46 +0100

    324324

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
+const inputAmount = document.querySelector('[name="amount"]');
+const inputStep = document.querySelector('[name="step"]');
+const inputDelay = document.querySelector('[name="delay"]');
+
+let count = 0;
+let interval = 0;
+
+form.addEventListener('submit', makePromisesOnClick);
+function makePromisesOnClick(ev) {
+  ev.preventDefault();
+
+  interval = Number(inputDelay.value);
+
+  const maker = interval => {
+    if (count < inputAmount.value) {
+      count += 1;
+      promisMake(inputDelay, inputStep, count, interval);
+    } else {
+      return clearInterval(timerId);
+    }
+  };
+  const timerId = setInterval(maker, interval);
+}
+// let  msDelay = Number(inputDelay.value);
+function promisMake(inputDelay, inputStep, count) {
+  const promis = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+  promis
+    .then(() => {
+      // msDelay += Number(inputStep.value);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${count} in ${interval}ms`);
+      interval += Number(inputStep.value);
+    })
+    .catch(() => {
+      // msDelay += Number(inputStep.value);
+      Notiflix.Notify.failure(`❌ Rejected promise ${count} in ${interval}ms`);
+      interval += Number(inputStep.value);
+    });
 }
